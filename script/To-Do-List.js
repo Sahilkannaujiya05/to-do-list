4a86de0bd1a13c9a1a4b90bfc29925bb5bf7802b
Sahilkannaujiya05 <sahilkannaujiya420@gmail.com>
2025-06-20 23:50:14 +0530

    Add files via upload

---
+++ script/To-Do-List.js
@@ -0,0 +1,55 @@
+const addToDoList = [
+            {value: 'make dinner',
+             dueDate: '2025-6-20'
+            },
+
+          {value: 'wash dishes',
+           dueDate: '2025-6-20'}];
+          function renderToDo(){
+          let addList = '';
+          for(let i = 0; i < addToDoList.length; i++){
+            const valueObject = addToDoList[i];
+            //name = valueObjec.name;
+            //dueDate = valuObjec.duedate;
+            const{value,dueDate} = valueObject;
+            const html = `
+              <div>${value}</div> 
+              <div> ${dueDate}</div> 
+              <button onclick = "
+              addToDoList.splice(${i}, 1);
+              renderToDo();
+                
+              "class = "delete-button"
+              >Delete</button>
+              `;
+            addList += html;
+          }
+
+          document.querySelector('.js-todo-score')
+          .innerHTML = addList;
+        }
+        document.querySelector('.js-add-button')
+        .addEventListener('click', () => {
+           addToDo();
+        });
+        /*document.body.addEventListener
+        ('keydown',(event)=>{
+          if(event.key === 'enter'){
+            addToDo()
+          }
+          });*/
+          function addToDo(){
+            const inputElem = document.querySelector('.js-input');
+            const value = inputElem.value;
+            const dueDateInputElem = document.querySelector('.js-dueDate');
+            const dueDate = dueDateInputElem.value;
+            
+            addToDoList.push({
+              //value: value,<...shorthand proerty
+              //dueDate: dueDate
+              value,
+              dueDate });
+            inputElem.value = '';
+            document.querySelector('.js-result');
+            renderToDo();
+          }
